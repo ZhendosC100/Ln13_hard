0f9098bb19683b2ba9df54f9fe02c84e77a2dc10
Yauhen Tretsyak <zhendos@tut.by>
2019-06-10 17:12:22 +0200

    gulp, optimazed the kod, change loading yandex map to main.js for optimization

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+const gulp = require('gulp'),
+    cleanCSS = require('gulp-clean-css'),
+    htmlmin = require('gulp-htmlmin'),
+    tinypng = require('gulp-tinypng-compress'),
+    jsmin = require('gulp-jsmin'),
+    rename = require('gulp-rename'),
+    pump = require('pump');
+
+
+gulp.task('minify-css', function(done){
+    return gulp.src('css/*.css') // from folder
+    .pipe(cleanCSS({
+        compatibility: 'ie8'
+    }))
+    .pipe(gulp.dest('dist/css/')); //to folder
+
+    done();
+});
+
+gulp.task('js-minify', function (cb) {
+    pump([
+          gulp.src(['js/*.js', '!js/*.min.js']),
+          jsmin(),
+          rename({suffix: '.min'}),
+          gulp.dest('dist/js/')
+      ],
+      cb
+    );
+  });
+
+gulp.task('move-js', function(done){
+    return gulp.src('js/*.min.js') // from folder
+    
+    .pipe(gulp.dest('dist/js/')); //to folder
+
+    done();
+});
+
+gulp.task('minify', () => {
+    return gulp.src('*.html')
+      .pipe(htmlmin({ collapseWhitespace: true }))
+      .pipe(gulp.dest('dist'));
+  });
+
+gulp.task('fonts', function(done){
+    return gulp.src('fonts/**/*') // from folder
+    
+    .pipe(gulp.dest('dist/fonts/')); //to folder
+
+    done();
+});
+
+gulp.task('tinypng', function (done) {
+    return gulp.src('./img/**/*.{png,jpg,jpeg}')
+        .pipe(tinypng({
+            key: 'vfVx0MDPzxL5uydUvkxm2WCyY5wIzBT6'
+        }))
+        .pipe(gulp.dest('dist/img/'));
+        done();
+});
+
+gulp.task('default', gulp.series('minify-css', 'js-minify','move-js' , 'minify', 'fonts','tinypng', function (done){
+    done();
+}));
+
--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
 
+    $(window).scroll(function(){
+        if($(window).scrollTop()>300){
+            $(".optimize").attr("src","https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A00eee1ff916de0a5ba4d755b92ae68598e4e5d4455b26dbde9c7a80a72e3f490&amp;width=100%25&amp;height=100%&amp;lang=ru_RU&amp;scroll=false&amp;id=ya_map&amp;apikey=0343c3a4-78d4-46cc-a0ae-66bd278f3d46");
+            $(".optimize").attr("type","text/javascript");
+            $(".optimize").attr("charset","utf-8");
+            $(".optimize").attr("async");
+        }
+    });
+
+    
+
 let btn = $('.navbar__btn'),
     modal = $('.modal'),
     up = $('.up'),
@@ -19,28 +30,77 @@ let btn = $('.navbar__btn'),
         $('html, body').animate({scrollTop:0});
     });
 
+// -------##### Form #####---------//
 
-    $("#form_offer").submit(function(e) {
+    let offerNameOn = $('.offer_name'),
+        offerPhoneOn = $('.offer_phone');
+    
+        $("#form_offer").submit(function(e) {
 
-        e.preventDefault(); // avoid to execute the actual submit of the form.
-    
-        var form = $(this);
-        var url = form.attr('action');
-    
-        $.ajax({
-               type: "POST",
-               url: url,
-               data: form.serialize(), // serializes the form's elements.
-               success: function(data)
-               {
-                   alert("Dane zostałe wysłane :)"); // show response from the php script.
-               },
-               error: function (data) {
-                alert("Niestety mamy błąd :(");
+            e.preventDefault(); // avoid to execute the actual submit of the form.
+        
+            var form = $(this);
+            var url = form.attr('action');
+
+            let offerName = offerNameOn.val(),
+                offerPhone = offerPhoneOn.val();
+
+            if(offerName != '' && offerPhone != ''){
+                $.ajax({
+                    type: "POST",
+                    url: url,
+                    data: form.serialize(), // serializes the form's elements.
+                    success: function(data)
+                    {
+                        alert("Dzęki! Dane zostały wysłane :)"); // show response from the php script.
+                    },
+                    error: function (data) {
+                     alert("Niestety mamy błąd :(");
+                 }
+                  });
+            } else {
+                alert('Заполните все поля!');
             }
-             });
-    
-    
-    });
+        
+            
+        
+        });
+
+        let brifNameOn = $('.brif_name'),
+            brifPhoneOn = $('.brif_phone'),
+            brifEmailOn = $('.brif_email');
+
+
+        $("#form_brif").submit(function(e) {
+
+            e.preventDefault(); // avoid to execute the actual submit of the form.
+        
+            var form = $(this);
+            var url = form.attr('action');
+
+            let brifName = brifNameOn.val(),
+                brifPhone = brifPhoneOn.val(),
+                brifEmail = brifEmailOn.val();
+
+            if(brifName != '' && brifPhone != '' && brifEmail != ''){
+                $.ajax({
+                    type: "POST",
+                    url: url,
+                    data: form.serialize(), // serializes the form's elements.
+                    success: function(data)
+                    {
+                        alert("Dzęki! Dane zostały wysłane :)"); // show response from the php script.
+                    },
+                    error: function (data) {
+                     alert("Niestety mamy błąd :(");
+                 }
+                  });
+            } else {
+                alert('Заполните все поля!');
+            }
+        
+            
+        
+        });
 
 });
